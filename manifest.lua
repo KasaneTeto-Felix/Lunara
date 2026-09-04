580764107cf2dc4803528410a2ab843a2b9c74c8
Kasane Felix <shirokoc328@gmail.com>
2026-09-04 17:17:10 +0700

    Create manifest.lua

---
+++ manifest.lua
@@ -0,0 +1,27 @@
+return {
+    { "src/startup.lua", "/startup.lua" },
+
+    { "src/lunara/boot.lua", "/lunara/boot.lua" },
+    { "src/lunara/kernel.lua", "/lunara/kernel.lua" },
+    { "src/lunara/vfs.lua", "/lunara/vfs.lua" },
+    { "src/lunara/shell.lua", "/lunara/shell.lua" },
+
+    { "src/bin/ls.lua", "/bin/ls" },
+    { "src/bin/pwd.lua", "/bin/pwd" },
+    { "src/bin/mkdir.lua", "/bin/mkdir" },
+    { "src/bin/touch.lua", "/bin/touch" },
+    { "src/bin/cat.lua", "/bin/cat" },
+    { "src/bin/echo.lua", "/bin/echo" },
+    { "src/bin/rm.lua", "/bin/rm" },
+    { "src/bin/cp.lua", "/bin/cp" },
+    { "src/bin/mv.lua", "/bin/mv" },
+    { "src/bin/clear.lua", "/bin/clear" },
+    { "src/bin/whoami.lua", "/bin/whoami" },
+    { "src/bin/hostname.lua", "/bin/hostname" },
+    { "src/bin/uname.lua", "/bin/uname" },
+    { "src/bin/uptime.lua", "/bin/uptime" },
+    { "src/bin/env.lua", "/bin/env" },
+    { "src/bin/help.lua", "/bin/help" },
+    { "src/bin/reboot.lua", "/bin/reboot" },
+    { "src/bin/shutdown.lua", "/bin/shutdown" }
+}
